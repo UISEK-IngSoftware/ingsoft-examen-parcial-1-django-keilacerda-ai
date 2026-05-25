1f53072082e0652e6cb72dd4ecdd022661697147
Keila Cerda Alvarado <keila.cerda@uisek.edu.ec>
2026-05-24 20:24:03 -0500

    Examen finalizado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+    synopsis = models.TextField()
+    poster = models.ImageField(
+        upload_to='movies/',
+        default='movies/default.jpg'
+    )
+
+    def __str__(self):
+        return self.title
+    
